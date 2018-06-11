4be72a15c15d0dc75d437dea052003e4c7a69d47
uhanson <uhanson@perlovka.org>
2018-06-11 14:07:15 +0300

    1st seemed working

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,10 @@
 {-# LANGUAGE TupleSections #-}
 module Main where
   import Data.Bifunctor
+  import Data.List
   import Text.Printf
-
-  type Idx = Int
-  type Size = Int
-  type Delta = Int
-
-  data Command
-    = Insert Idx Size
-    | Delete Idx Size
-    | Move Idx Size Idx 
-    deriving (Eq, Show)
-
-  data Step
-    = Shift Idx Size Delta
-    | Del Idx Size
-    deriving (Eq, Show)
-
-  data I = I !Delta !Bool
-    deriving (Eq, Show)
   
-  ranges = map (\i -> (i, 0)) ([1..16] :: [Integer])
+  ranges = map (\i -> (i, 0)) ([1..16] :: [Int])
 
   both f = bimap f f
 
@@ -29,16 +12,15 @@ module Main where
     where 
       shift t@(i, _) = if i >= idx then both (+1) t else t
 
-  delete idx size = filter del
+  del idx size = filter (check . fst)
     where 
-      del (i, _) = i < idx || i > idx + size - 1
+      check i = i < idx || i > idx + size - 1
   
   move from size to = map shift
     where
-      (ds, ml, mr) = if from > to 
-        then (size, to, from - 1)
-        else (negate size, from + size, to - 1)
-      di = to - from + ds
+      (di, ds, ml, mr) = if from > to 
+        then (to - from, size, to, from - 1)
+        else (to - from - size, negate size, from + size, to - 1)
       shift t@(i, _) | i >= from && i <= from + size - 1    = both (+ di) t
                      | i >= ml && i <= mr                   = both (+ ds) t
                      | otherwise                            = t
@@ -47,6 +29,6 @@ module Main where
 
   main :: IO ()
   main = do 
-    let commands = move 11 4 3 -- . delete 6 2 . move 5 4 13
-        processed = commands ranges
+    let commands = move 11 4 3 . del 6 2 . move 5 4 13
+        processed = sortOn fst (commands ranges)
     mapM_ (putStrLn . showElem) processed
